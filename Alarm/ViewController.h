192e6283b73dc4d1d871ca1ffddc178b90645795
Bursuc Andrei <andrei.bursuc@thinslices.com>
2017-04-10 09:05:12 +0300

    Initial Commit

---
+++ Alarm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alarm
+//
+//  Created by Bursuc Andrei on 4/10/17.
+//  Copyright © 2017 Andrei Bursuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Alarm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alarm
+//
+//  Created by Bursuc Andrei on 4/10/17.
+//  Copyright © 2017 Andrei Bursuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
